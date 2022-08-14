39e77c58d6f7c7418c6fcebdd46a5583bffdfa56
Daniel Kamasz <kamaszdaniel@gmail.com>
2022-08-14 17:26:30 +0200

    First commit

---
+++ src/main/java/Kalkulator.java
@@ -0,0 +1,16 @@
+public class Kalkulator {
+    int a;
+    int b;
+
+    public int addAtoB(int a, int b){
+        this.a = a;
+        this.b = b;
+        return a + b;
+    }
+
+    public int substractAfromB(int a, int b) {
+        this.a = a;
+        this.b = b;
+        return a - b;
+    }
+}
